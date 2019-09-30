6e1cb7750c7c83a0f29608a5331154ae72c55cb4
enterdoki <chen.tommy925@gmail.com>
2019-09-30 18:35:46 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# API for Stock Portfolio App
---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res, next) => {
+    res.status(200).send("Default API route.");
+})
+
+app.listen(port, () => {
+    console.log(`Sever listening on port: ${port}`); 
+})
+
