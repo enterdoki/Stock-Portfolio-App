0940e5c5f92a5f91c7f7551e0ce19b4507cd3fef
enterdoki <chen.tommy925@gmail.com>
2019-10-04 20:36:57 -0400

    find user

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const authRoute = require('./auth');
 const stockRoute = require('./stock');
+const userRoute = require('./user');
 
 router.use('/auth', authRoute);
 router.use('/stock', stockRoute);
+router.use('/user', userRoute);
+
 
 router.use((req, res, next) => {
     res.status(200).send("Default API route.");
---
+++ routes/user.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const user = express.Router();
+const User = require('../database/models/user')
+
+user.use(bodyParser.json());
+
+user.get('/:email', async(req, res, next) => {
+    try {
+        const data = await User.findOne({ where: { email: req.params.email }, attributes: {exclude: ['password']} });
+        if(data) {
+            res.status(200).send(data);
+        } else {
+            res.status(400).send('User not found.')
+        }
+    }catch(err) {
+        res.status(400).send(err);
+    }
+})
+
+module.exports = user;
